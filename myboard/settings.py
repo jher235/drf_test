6a3b4b583d587b35a7f33841fd48f31be549a79b
jher235 <tim668666@gmail.com>
2023-07-08 15:31:19 +0900

    t2

--- myboard/settings.py
+++ myboard/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))        
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True        #ㅂㅍ
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
